928b4c8540b1e732a7f503c479a4e446baead3ae
Zachary Kubli <kubli.zachary@gmail.com>
2025-04-17 13:03:49 -0400

    three functions

--- wiki_network.py
+++ wiki_network.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+# WIKI FETCH AND LINK EXTRACTION
+
 # Get the HTML from a Wikipedia article
 def fetch_html(url):
     html = ""
@@ -14,3 +16,32 @@ def extract_links(html, max_links=10):
     # TODO: Use BeautifulSoup to find valid /wiki/ links without colons
     print("Extracted links:", links)
     return links
+
+# GRAPH CREATION
+import networkx as nx
+
+# Build a directed graph from a root node and its outgoing links
+def build_graph(root_title, linked_titles):
+    G = nx.DiGraph()
+    # TODO: Add root node and edges from root to each linked title
+    print("Graph nodes:", G.nodes())
+    print("Graph edges:", G.edges())
+    return G
+
+# Print graph summary and return a simple object
+def format_graph_output(graph):
+    summary = {
+        "nodes": 0,
+        "edges": 0,
+        "edge_list": []
+    }
+    # TODO: Fill in node count, edge count, and list of edges
+    print("Graph summary:", summary)
+    return summary
+
+def make_wiki_graph(root_url):
+    html = fetch_html(root_url)
+    links = extract_links(html)
+    root_title = root_url.split("/")[-1].replace("_", " ")
+    graph = build_graph(root_title, links)
+    return format_graph_output(graph)
