8aef6a12728a152426a7fea8327f58c15bdf1889
Zachary Kubli <kubli.zachary@gmail.com>
2025-04-17 13:33:14 -0400

    graph building

--- wiki_network.py
+++ wiki_network.py
@@ -19,11 +19,15 @@ def extract_links(html, max_links=10):
 
 # GRAPH CREATION
 import networkx as nx
+    
 
 # Build a directed graph from a root node and its outgoing links
 def build_graph(root_title, linked_titles):
     G = nx.DiGraph()
-    # TODO: Add root node and edges from root to each linked title
+    G.add_node(root_title)
+    for title in linked_titles:
+        G.add_node(title)
+        G.add_edge(root_title, title)
     print("Graph nodes:", G.nodes())
     print("Graph edges:", G.edges())
     return G
@@ -35,7 +39,9 @@ def format_graph_output(graph):
         "edges": 0,
         "edge_list": []
     }
-    # TODO: Fill in node count, edge count, and list of edges
+    summary["nodes"] = graph.number_of_nodes()
+    summary["edges"] = graph.number_of_edges()
+    summary["edge_list"] = list(graph.edges()) # nx.to_edgelist(graph) #
     print("Graph summary:", summary)
     return summary
 
@@ -45,3 +51,26 @@ def make_wiki_graph(root_url):
     root_title = root_url.split("/")[-1].replace("_", " ")
     graph = build_graph(root_title, links)
     return format_graph_output(graph)
+
+def make_deep_wiki_graph(root_url, depth=1):
+    html = fetch_html(root_url)
+    links = extract_links(html)
+    root_title = root_url.split("/")[-1].replace("_", " ")
+    graph = build_graph(root_title, links)
+    
+    for _ in range(depth):
+        new_links = []
+        for node in list(graph.nodes()):
+            html = fetch_html(f"https://en.wikipedia.org/wiki/{node.replace(' ', '_')}")
+            new_links += extract_links(html)
+        new_links = list(set(new_links) - set(graph.nodes()))
+        graph.add_nodes_from(new_links)
+        for title in new_links:
+            graph.add_edge(node, title)
+    
+    return format_graph_output(graph)
+
+if __name__ == "__main__":
+    root_url = "https://en.wikipedia.org/wiki/Computer_programming"
+    print(make_wiki_graph(root_url))
+    print(make_deep_wiki_graph(root_url, depth=2))
